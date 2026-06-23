29beb261788708fbbad0501225840381489f9ec1
selimdawa <selimdawa@gmail.com>
2026-06-23 06:07:47 +0300

    Libs Update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,24 +19,32 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
-    signingConfigs {
-        create("release") {
-            storeFile = file("D:\\MyProjects\\Kotlin\\Little Note\\Little Note\\LittleNote.jks")
-            storePassword = "00000000"
-            keyAlias = "LittleNote"
-            keyPassword = "00000000"
-        }
-    }
+    //signingConfigs {
+    //    create("release") {
+    //        storeFile = file("D:\\MyProjects\\Kotlin\\Little Note\\Little Note\\LittleNote.jks")
+    //        storePassword = "00000000"
+    //        keyAlias = "LittleNote"
+    //        keyPassword = "00000000"
+    //    }
+    //}
     buildTypes {
-        getByName("release") {
-            signingConfig = signingConfigs.getByName("release")
-            isMinifyEnabled = true
-            isShrinkResources = true
+        release {
+            isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"
             )
         }
     }
+    //buildTypes {
+    //    getByName("release") {
+    //        signingConfig = signingConfigs.getByName("release")
+    //        isMinifyEnabled = true
+    //        isShrinkResources = true
+    //        proguardFiles(
+    //            getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"
+    //        )
+    //    }
+    //}
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
@@ -62,16 +70,10 @@ dependencies {
     //Layout
     implementation(libs.androidx.constraintlayout)
     implementation(libs.material)
-    implementation(libs.androidx.legacy.support.v4)
-    implementation(libs.androidx.cardview)
     //Firebase
     implementation(platform(libs.firebase.bom))            //Firebase BOM
     implementation(libs.firebase.auth)
     implementation(libs.firebase.analytics)
     implementation(libs.firebase.firestore)                //Firebase Fire Store
-    //Don't Change
     implementation(libs.firebase.ui.firestore)             //Firebase Store UI
-    //implementation(libs.firebase.database)
-    //implementation(libs.firebase.storage)
-    //implementation(libs.firebase.crashlytics)
 }
