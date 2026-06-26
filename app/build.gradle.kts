0833a94b2387e247492f636d07d44c707d7102f7
selimdawa <selimdawa@gmail.com>
2026-06-27 02:48:36 +0300

    viewBinding Add

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,14 +48,10 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
-    }
-    kotlin {
-        compilerOptions {
-            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+
+        buildFeatures {
+            viewBinding = true
         }
-    }
-    buildFeatures {
-        dataBinding = true
     }
 }
 
