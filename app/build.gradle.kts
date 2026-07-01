b5f514d549e87d9fa5591d192588ee2a9b6992c5
selimdawa <selimdawa@gmail.com>
2026-07-01 23:05:09 +0300

    statusBarColor - Fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,10 +48,9 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
-
-        buildFeatures {
-            viewBinding = true
-        }
+    }
+    buildFeatures {
+        viewBinding = true
     }
 }
 
