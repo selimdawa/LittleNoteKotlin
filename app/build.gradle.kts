03baaf3833dce44ef0c7930708e86f782acb346a
selimdawa <selimdawa@gmail.com>
2026-08-31 14:55:55 +0300

    Toml - Update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,7 +63,6 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     //Layout
-    implementation(libs.androidx.constraintlayout)
     implementation(libs.material)
     //Firebase
     implementation(platform(libs.firebase.bom))            //Firebase BOM
