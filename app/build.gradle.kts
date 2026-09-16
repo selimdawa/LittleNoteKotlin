e4acf7eb5a049a139465bf964f661e19b37219e6
selimdawa <selimdawa@gmail.com>
2026-09-16 20:12:06 +0300

    Clean Code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,17 +47,13 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.activity)
-    implementation(libs.androidx.datastore.preferences)   //DataStore
+    implementation(libs.androidx.fragment)
     //Layout
     implementation(libs.material)
     implementation(libs.multicolors)
-    //Image
-    implementation(libs.coil3.main)
-    implementation(libs.coil3.network)
     //Firebase
     implementation(platform(libs.firebase.bom))            //Firebase BOM
     implementation(libs.firebase.auth)
-    implementation(libs.firebase.analytics)
     implementation(libs.firebase.firestore)                //Firebase Fire Store
     //MVVM
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
@@ -84,10 +80,6 @@ dependencies {
     ksp(libs.androidx.hilt.compiler)
     //Biometric
     implementation(libs.androidx.biometric)
-    //Test
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
 }
 
 ksp {
